020679e2f355285cb780f42a4070c420be97982a
Pnkj124 <chaudharypankaj.official@gmail.com>
2021-10-29 15:07:43 +0200

    update image in profile page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -8,6 +8,7 @@ import { Component} from '@angular/core';
 export class ProfilePage{
 
   user: string = 'Pankaj Chaudhary';
+  userProfileImage: string = "https://jimswbckxyazqwvjqsdg.supabase.in/storage/v1/object/sign/roskilde-places/profile/profile_pic.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyb3NraWxkZS1wbGFjZXMvcHJvZmlsZS9wcm9maWxlX3BpYy5qcGciLCJpYXQiOjE2MzU1MTI3MTYsImV4cCI6MTk1MDg3MjcxNn0.5J-VXL_WBI756lHgMfYoRMNobyXjVj7XRTRrnn9O1TM";
   constructor() { }
 
 }
